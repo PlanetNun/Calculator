75024f8692f966df68a28fb9baeb8f54892178af
Sebin Kim <nccsmart89@naver.com>
2023-12-24 10:11:26 -0800

    First Demo Calculator

--- javascript/main.js
+++ javascript/main.js
@@ -1,54 +1,186 @@
-let initValue = 0;
-let selectedValue;
-let tempMemory = [];
+let equalCount = 0;
+let addCount = 0;
+let plusUse = 0;
+let subtractUse = 0;
+let tempMemoryNew = [];
+let tempMemoryOld = [];
+let tempMemoryResult = [];
+let tempJoin = [];
+
+function blankDisplay() {
+  document.getElementById("displayPanel").innerHTML = "";
+}
+function clearButton() {
+  tempMemoryNew = [];
+  tempMemoryOld = [];
+  addCount = 0;
+  equalCount = 0;
+  document.getElementById("displayPanel").innerHTML = "";
+}
+function initializeDisplay() {
+  if (equalCount !== 0) {
+    tempMemoryNew = [];
+    addCount = 0;
+    equalCount = 0;
+    document.getElementById("displayPanel").innerHTML = "";
+  }
+  if (addCount === 0) {
+    blankDisplay();
+  }
+}
+
+function preProcessor() {
+  let num = Number(tempMemoryNew.join(""));
+  tempMemoryOld.push(num);
+  tempMemoryNew = [];
+}
+
 function clickNine() {
+  initializeDisplay();
+
   document.getElementById("displayPanel").innerHTML += 9;
-  tempMemory.push(9);
+
+  tempMemoryNew.push(9);
+  addCount++;
+
 }
 function clickEight() {
-  document.getElementById("displayPanel").innerHTML = 8;
-  tempMemory.push(8);
+  initializeDisplay();
+
+  document.getElementById("displayPanel").innerHTML += 8;
+  tempMemoryNew.push(8);
+  addCount++;
 }
 function clickSeven() {
-  document.getElementById("displayPanel").innerHTML = 7;
-  tempMemory.push(7);
+  initializeDisplay();
+
+  document.getElementById("displayPanel").innerHTML += 7;
+  tempMemoryNew.push(7);
+  addCount++;
 }
 function clickSix() {
-  document.getElementById("displayPanel").innerHTML = 6;
-  tempMemory.push(6);
+  initializeDisplay();
+
+  document.getElementById("displayPanel").innerHTML += 6;
+  tempMemoryNew.push(6);
+  addCount++;
 }
 function clickFive() {
-  document.getElementById("displayPanel").innerHTML = 5;
-  tempMemory.push(5);
+  initializeDisplay();
+
+  document.getElementById("displayPanel").innerHTML += 5;
+  tempMemoryNew.push(5);
+  addCount++;
 }
 function clickFour() {
-  document.getElementById("displayPanel").innerHTML = 4;
-  tempMemory.push(4);
+  initializeDisplay();
+
+  document.getElementById("displayPanel").innerHTML += 4;
+  tempMemoryNew.push(4);
+  addCount++;
 }
 function clickThree() {
-  document.getElementById("displayPanel").innerHTML = 3;
-  tempMemory.push(3);
+  initializeDisplay();
+
+  document.getElementById("displayPanel").innerHTML += 3;
+  tempMemoryNew.push(3);
+  addCount++;
 }
 function clickTwo() {
-  document.getElementById("displayPanel").innerHTML = 2;
-  tempMemory.push(2);
+  initializeDisplay();
+
+  document.getElementById("displayPanel").innerHTML += 2;
+  tempMemoryNew.push(2);
+  addCount++;
 }
 function clickOne() {
-  document.getElementById("displayPanel").innerHTML = 1;
-  tempMemory.push(1);
+  initializeDisplay();
+
+  document.getElementById("displayPanel").innerHTML += 1;
+  tempMemoryNew.push(1);
+  addCount++;
 }
 function clickZero() {
-  document.getElementById("displayPanel").innerHTML = 0;
-  tempMemory.push(0);
+  initializeDisplay();
+
+  document.getElementById("displayPanel").innerHTML += 0;
+  tempMemoryNew.push(0);
+  addCount++;
 }
 
-function add(tempMemory) {
-  for (let i = 0; i < tempMemory.length; i++) {
-    let sum = 0;
-    sum += tempMemory[i];
+function add() {
+  let num = Number(tempMemoryNew.join(""));
+  tempMemoryOld.push(num);
+  tempMemoryNew = [];
+
+  addCount = 0;
+  let result = 0;
+
+  for (let i = 0; i < tempMemoryOld.length; i++) {
+    result += tempMemoryOld[i];
   }
-  document.getElementById("displayPanel").innerHTML = sum;
+  if (equalCount !== 0) {
+    blankDisplay();
+  }
+  tempMemoryOld = [];
+  tempMemoryOld.push(result);
+
+  document.getElementById("displayPanel").innerHTML = result;
+  operatorUse++;
 }
 
+function subtract() {
+  let num = -Number(tempMemoryNew.join(""));
+  tempMemoryOld.push(num);
+  tempMemoryNew = [];
+  addCount = 0;
+  let result = 0;
+  for (let i = 0; i < tempMemoryOld.length; i++) {
+    result += tempMemoryOld[i];
+  }
+  if (equalCount !== 0) {
+    blankDisplay();
+  }
+  tempMemoryOld = [];
+  tempMemoryOld.push(result);
+  document.getElementById("displayPanel").innerHTML = result;
+}
 
-console.log(tempMemory);
+function timeMath() {
+  let num = Number(tempMemoryNew.join(""));
+  tempMemoryOld.push(num);
+  tempMemoryNew = [];
+  addCount = 0;
+  let initValue = 1;
+  for (let i = 0; i < tempMemoryOld.length; i++) {
+    initValue *= tempMemoryOld[i];
+  } if (equalCount !== 0) {
+    blankDisplay();
+  }
+  tempMemoryOld = [];
+  tempMemoryOld.push(initValue);
+  document.getElementById("displayPanel").innerHTML = initValue;
+}
+
+function divideFunc() {
+  let num = Number(tempMemoryNew.join(""));
+  tempMemoryOld.push(num);
+  tempMemoryNew = [];
+  addCount = 0;
+  let initValue = 1;
+  for (let i = 0; i < tempMemoryOld.length; i++) {
+    initValue /= tempMemoryOld[i];
+  } if (equalCount !== 0) {
+    blankDisplay();
+  }
+  tempMemoryOld = [];
+  tempMemoryOld.push(initValue);
+  document.getElementById("displayPanel").innerHTML = initValue;
+}
+
+function equalTo() {
+  equalCount++;
+  let result = tempMemoryOld[0];
+  document.getElementById("displayPanel").innerHTML = result;
+}
+
